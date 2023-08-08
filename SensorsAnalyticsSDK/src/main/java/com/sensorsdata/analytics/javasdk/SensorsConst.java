cedbe229c331b466b5033d90144e12f7f58a183c
weiqiangliu <liuweiqiang@sensorsdata.cn>
2023-08-08 16:32:14 +0800

    Release 3.6.5

--- SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/javasdk/SensorsConst.java
+++ SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/javasdk/SensorsConst.java
@@ -15,7 +15,7 @@ public class SensorsConst {
     /**
      * 当前JDK版本号，注意要和pom文件里面的version保持一致
      */
-    public static final String SDK_VERSION = "3.6.4";
+    public static final String SDK_VERSION = "3.6.5";
     /**
      * 当前语言类型
      */
